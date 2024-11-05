c5046b89cf9f01b1c4a92032591abc31b95a7201
sedatattiko <sedat@tiko.es>
2024-11-05 19:23:51 +0100

    Add data access for rest of the services

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,4 +1,15 @@
+using CloudWeather.Report.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<WeatherReportDbContext>(
+  opts =>
+  {
+    opts.EnableSensitiveDataLogging();
+    opts.EnableDetailedErrors();
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+  }, ServiceLifetime.Transient);
 
 var app = builder.Build();
 
